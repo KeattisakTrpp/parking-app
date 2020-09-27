c7f7bbe688e3c678cbc49186bdde6e227d253d33
keattisak <keattisak.trpp@gmail.com>
2020-09-27 23:10:31 +0700

    test heroku

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan')
 
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
